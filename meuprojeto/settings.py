9011733c750a35641a575fbedf3badff10fb2341
Adilson Lucas da Silva <alslucas19@gmail.com>
2026-03-26 16:13:40 -0300

    Preparando deploy no Render

--- meuprojeto/settings.py
+++ meuprojeto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-o1s1m4vd^ju#=d6k4&k+5@15s=di8gs#1d*mdan-s-68u-m-&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -124,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
