c7458888797da3ba0925c6afb99a16f5e4f15fb0
Adilson Lucas da Silva <24205869@aluno.univesp.br>
2026-04-03 23:10:55 -0300

    Revert "muda visual da pagina de login para fundo pink"
    This reverts commit a1fc9ec5b12275dc8a3c9edf6ef5e96d6acef676.

--- meuprojeto/settings.py
+++ meuprojeto/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'meuprojeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carteira_pet',   # O nome do banco que você criou no Workbench
+        'USER': 'root',           # Seu usuário MySQL
+        'PASSWORD': 'amor1919',  # Sua senha MySQL
+        'HOST': 'localhost',      # Ou o IP do servidor (ex: '127.0.0.1')
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -118,4 +126,3 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
